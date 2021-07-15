b9411ba60a1f6e16a4a755ee73068ad98e7b29ce
Havanur <havanurtosun0618@gmail.comhavanurtosun0618@gmail.com>
2021-07-15 19:28:53 -0400

    commit

--- app.js
+++ app.js
@@ -17,11 +17,11 @@ function play() {
 
         if(userGuess < answer) {
             msg1.textContent = "Your guess is too low";
-            msg2.textContent = "No.of guesses: " + noOfGuesses;
+            msg2.textContent = "# Of guesses: " + noOfGuesses;
             msg3.textContent = "Guessed Numbers are: " + guessedNums;
         } else if (userGuess > answer){
             msg1.textContent = "Your guess is too high";
-            msg2.textContent = "No.of guesses: " + noOfGuesses;
+            msg2.textContent = "# Of guesses: " + noOfGuesses;
             msg3.textContent = "Guessed Numbers are: " + guessedNums;
         } else if (userGuess == answer) {
             msg1.textContent = "🎉 YUPPII YOU WIN! 🎉";
@@ -34,4 +34,32 @@ function play() {
 
 reset.addEventListener("click", function(){
     location.reload();
+});
+
+
+document.addEventListener("keyup", function (event){
+    if (event.keyCode == 13) {
+        let userGuess = document.getElementById("guess").value;
+    // console.log(userGuess);
+    if ( userGuess < 1 || userGuess > 100) {
+        alert("Please enter a number between 1 to 100!");
+    } else {
+        guessedNums.push(userGuess);
+        noOfGuesses += 1;
+
+        if(userGuess < answer) {
+            msg1.textContent = "Your guess is too low";
+            msg2.textContent = "# Of guesses: " + noOfGuesses;
+            msg3.textContent = "Guessed Numbers are: " + guessedNums;
+        } else if (userGuess > answer){
+            msg1.textContent = "Your guess is too high";
+            msg2.textContent = "# Of guesses: " + noOfGuesses;
+            msg3.textContent = "Guessed Numbers are: " + guessedNums;
+        } else if (userGuess == answer) {
+            msg1.textContent = "🎉 YUPPII YOU WIN! 🎉";
+            msg2.textContent = "The number was: " + answer;
+            msg3.textContent = "You guessed it in " + noOfGuesses;
+        }
+    }
+    }
 });
